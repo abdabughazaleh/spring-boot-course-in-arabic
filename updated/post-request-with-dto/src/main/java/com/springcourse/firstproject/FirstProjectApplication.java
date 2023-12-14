15c0cec06ae9aa1754bd949dd21fa9ca231e47f6
Abd AbuGhazaleh <abdabughazaleh@hotmail.com>
2023-12-15 02:12:57 +0300

    add Create Post request with DTO

---
+++ updated/post-request-with-dto/src/main/java/com/springcourse/firstproject/FirstProjectApplication.java
@@ -0,0 +1,14 @@
+package com.springcourse.firstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class FirstProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstProjectApplication.class, args);
+    }
+
+}
---
+++ updated/post-request-with-dto/src/test/java/com/springcourse/firstproject/FirstProjectApplicationTests.java
@@ -0,0 +1,18 @@
+package com.springcourse.firstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectApplicationTests {
+
+    @Test
+    void contextLoads()  {
+    }
+
+    @Test
+    void test(){
+        System.out.println("Test Test method .....");
+    }
+
+}
