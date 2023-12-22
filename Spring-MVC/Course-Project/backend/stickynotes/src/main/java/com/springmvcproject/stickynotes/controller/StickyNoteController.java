b49f280898bfe0221763682e594bc4e877620e02
Abd AbuGhazaleh <abdabughazaleh@hotmail.com>
2023-12-22 17:18:15 +0300

    add project pages

--- Spring-MVC/Course-Project/backend/stickynotes/src/main/java/com/springmvcproject/stickynotes/controller/StickyNoteController.java
+++ Spring-MVC/Course-Project/backend/stickynotes/src/main/java/com/springmvcproject/stickynotes/controller/StickyNoteController.java
@@ -2,11 +2,21 @@ package com.springmvcproject.stickynotes.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class StickyNoteController {
     @GetMapping("")
-    public String test() {
-        return "test";
+    public String home() {
+        return "home";
     }
+    @GetMapping("my-notes")
+    public String myNotes() {
+        return "my-notes";
+    }
+    @GetMapping("sticky-note/{id}")
+    public String stickyNote(@PathVariable(name = "id") Integer id) {
+        return "sticky-note";
+    }
+
 }
